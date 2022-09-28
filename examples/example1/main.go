8c55b6f5655958deed489f47f4e95e2bc839d628
Dmitry Tkachenko <tdv@ngs.ru>
2022-09-28 23:37:47 +0300

    A little fix

--- examples/example1/main.go
+++ examples/example1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/tdv/go-pool/pool"
@@ -16,13 +17,19 @@ func main() {
 	p := pool.New(ctx, 5)
 	defer p.Stop()
 
+	wg := sync.WaitGroup{}
+
 	for i := 0; i < 20; i++ {
 		i := i
+		wg.Add(1)
+
 		p.Go(func(context.Context) {
+			defer wg.Done()
 			fmt.Println(i)
-			time.Sleep(time.Second * 8)
+			time.Sleep(time.Second * 2)
 		})
 	}
 
-	time.Sleep(time.Second * 20)
+	time.Sleep(time.Second)
+	wg.Wait()
 }
